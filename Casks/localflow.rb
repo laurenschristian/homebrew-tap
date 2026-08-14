bddc4d8a8639e5eed7e6f42a70f3fb18ac4d9419
Lawrence Christian <67164412+laurenschristian@users.noreply.github.com>
2026-08-14 10:24:32 -0500

    fix: localflow cask points at 0.8.0 dmg

--- Casks/localflow.rb
+++ Casks/localflow.rb
@@ -1,8 +1,8 @@
 cask "localflow" do
   version "0.8.0"
-  sha256 "7cb727908e8e7d6f1fbeabec16ab4eb4db8286786bda173f8657f1a8b1750109"
+  sha256 "301c165564cb149f416ce8d07e2d5c76e6981eb49f284f2161021291cf2be9ee"
 
-  url "https://github.com/laurenschristian/local-flow/releases/download/v#{version}/LocalFlow-#{version}.zip"
+  url "https://github.com/laurenschristian/local-flow/releases/download/v#{version}/LocalFlow-v#{version}-mac-arm64.dmg"
   name "LocalFlow"
   desc "Local Whisper dictation from the menu bar"
   homepage "https://github.com/laurenschristian/local-flow"
