3a7c3e4dc790736cc0711db326b93cff8da81e55
Lawrence Christian <67164412+laurenschristian@users.noreply.github.com>
2026-08-20 11:30:13 -0500

    chore: localflow 0.12.1

--- Casks/localflow.rb
+++ Casks/localflow.rb
@@ -1,6 +1,6 @@
 cask "localflow" do
-  version "0.12.0"
-  sha256 "0cd452d6d88d6e25e3803f1165707e30efa6e0e8bb24e2510db8c3a08304dfb2"
+  version "0.12.1"
+  sha256 "2f71e72b7711d8e0978584fe3de226c54c2cbfa882ef976ba659f0b279128e7d"
 
   url "https://github.com/laurenschristian/local-flow/releases/download/v#{version}/LocalFlow-v#{version}-mac-arm64.dmg"
   name "LocalFlow"
